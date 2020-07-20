4ac28c9164ab4331d96e73fcaf46e9db68901cb1
amolwabale <a.m.wabale@gmail.com>
2020-07-20 21:46:38 +0530

    added cmd design pattern

---
+++ DesignPattern/CommandDesignPattern/CommandGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDesignPattern
+{
+    public class CommandGroup
+    {
+        private List<IExecuter> _cmdList = new List<IExecuter>();
+
+        public void AddCommand(IExecuter cmd)
+        {
+            _cmdList.Add(cmd);
+        }
+
+        public void ExecuteCommand()
+        {
+            foreach (var cmdObj in _cmdList)
+            {
+                cmdObj.Execute();
+            }
+        }
+    }
+}
---
+++ DesignPattern/CommandDesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandGroup obj = new CommandGroup();
+            obj.AddCommand(new Close());
+            obj.AddCommand(new Open());
+            obj.AddCommand(new Print());
+            obj.ExecuteCommand();
+        }
+    }
+}
