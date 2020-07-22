0116a16874f8f9d143c822d6912af3c0edb8d1b9
amolwabale <a.m.wabale@gmail.com>
2020-07-22 23:38:50 +0530

    added seudocode

---
+++ PseudoCode/ZS.Array.ProblemStatement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZS.Array.ProblemStatement
+{
+    class Program
+    {
+        //Problem Statement
+        //If there is two array
+        //string[] Arr1 = { "mango", "apple", "grape", "fig" };
+        //string[] Arr2 = { "pineapple", "pomegranate", "papaya", "apple" };
+        //need all items from first array and from second array only those which is not present in first with less time complexity.
+
+        static void Main(string[] args)
+        {
+            string[] Arr1 = { "mango", "apple", "grape", "fig", "apple" };
+            string[] Arr2 = { "pineapple", "pomegranate", "papaya", "apple" };
+            Evaluate(Arr1,Arr2);
+        }
+
+        public static void Evaluate(string[] arr1, string[] arr2)
+        {
+            var a = arr1.Intersect(arr2).ToArray();
+            var result = arr1.Concat(arr2.Except(a).ToArray());
+            
+        }
+    }
+}
