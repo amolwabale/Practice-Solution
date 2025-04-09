a67a56b52d70966bda4269c41b1dd84c5d1a7bf0
Amol Wabale <a.m.wabale@gmail.com>
2025-04-09 22:24:45 +0530

    added changes

---
+++ PseudoCode/DataStructure/DataStructure.Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 5, 6, 7 };
+            var data = FindMissingNumberFromArray(arr);
+        }
+
+        public static int FindMissingNumberFromArray(int[] arr)
+        {
+            var sum = 0;
+            var arrSum = 0;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                sum += i+1;
+                arrSum += arr[i];
+            }
+            return Math.Abs(arrSum - sum);
+        }
+    }
+}
---
+++ PseudoCode/DataStructure/DataStructure.Array/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataStructure.Array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ZS Associates")]
+[assembly: AssemblyProduct("DataStructure.Array")]
+[assembly: AssemblyCopyright("Copyright © ZS Associates 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("933b5987-ce3f-4092-bd42-73d6109d84d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
